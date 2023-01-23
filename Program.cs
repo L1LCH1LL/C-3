cb3ec66ee665a662d96f22ffe6e82bf2a1c334cd
L1LCH1LL <108305081+L1LCH1LL@users.noreply.github.com>
2023-01-23 22:54:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+int n1 = number % 10;
+int n2 = (number % 100) / 10; 
+int n4 = number / 1000 % 10; 
+int n5 = number / 10000;
 
-for (int i = 1; i < num+1; i++)
+if (n1 == n5 & n2 == n4)
 {
-    Console.WriteLine(i*i*i);
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+
+else
+{
+    Console.WriteLine($"Число {number} не является палиндромом");
 }
 
 
